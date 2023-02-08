c033d0994d930483ee56bc6223cec0ffb5b0b403
Jacob Simmons-Rosen <jsimmonsrosen@gmail.com>
2023-02-08 09:37:46 -0500

    Moves components and tidies up nav

--- GiftCardXChange/src/app/app-routing.module.ts
+++ GiftCardXChange/src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BrandComponent } from './brand/brand.component';
-import { LoginComponent } from 'src/login/login.component';
-import { RegisterComponent } from 'src/register/register.component';
+import { LoginComponent } from 'src/app/login/login.component';
+import { RegisterComponent } from 'src/app/register/register.component';
 
 const routes: Routes = [
   {path: 'brand', component: BrandComponent},
