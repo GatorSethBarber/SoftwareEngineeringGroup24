8c262bc603d6312e4ce4862b004f9b6d3d2231c2
Jacob Simmons-Rosen <jsimmonsrosen@gmail.com>
2023-02-15 15:05:58 -0500

    Adds image to card table

--- GiftCardXChange/src/app/cards/cards.component.ts
+++ GiftCardXChange/src/app/cards/cards.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router, ActivatedRoute, NavigationStart } from '@angular/router';
 import { CARDS } from '../mock-cards';
+import { BRANDS } from '../mock-brands';
 
 @Component({
   selector: 'app-cards',
@@ -11,6 +12,9 @@ export class CardsComponent {
 
   public state = "";
   cards = CARDS;
+  brands = BRANDS;
+  chosenBrand: import("../brand").Brand;
+
 
   constructor(
     private route: ActivatedRoute,
@@ -19,6 +23,12 @@ export class CardsComponent {
 
   ngOnInit() {
     this.state = window.history.state.brandName;
+
+    this.brands.forEach(brand => {
+      if (brand.name == this.state) {
+         this.chosenBrand = brand;
+      }
+    });
   }
 
 }
