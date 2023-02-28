a0697600e63ac23d111eb88d95583ac1fc942df9
Jacob Simmons-Rosen <jsimmonsrosen@gmail.com>
2023-02-28 11:15:39 -0500

    Changes cards to get based on database

--- GiftCardXChange/src/app/login/login.component.ts
+++ GiftCardXChange/src/app/login/login.component.ts
@@ -29,6 +29,9 @@ export class LoginComponent {
   });
 
   onSubmit() {
+
+    console.log(this.loginForm.value)
+
     this.AuthService.login(this.loginForm.value).subscribe(
       (res) => {
         console.log(res);
