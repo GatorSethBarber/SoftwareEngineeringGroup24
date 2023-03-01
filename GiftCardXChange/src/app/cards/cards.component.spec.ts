677a7854ba831bffad4591a4597c2ebb31a7eb6c
Jacob Simmons-Rosen <jsimmonsrosen@gmail.com>
2023-03-01 16:02:16 -0500

    Fixes preliminary unit tests

--- GiftCardXChange/src/app/cards/cards.component.spec.ts
+++ GiftCardXChange/src/app/cards/cards.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CardsComponent } from './cards.component';
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('CardsComponent', () => {
   let component: CardsComponent;
@@ -8,9 +13,14 @@ describe('CardsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CardsComponent ]
+      declarations: [ CardsComponent ],
+      imports: [ RouterTestingModule, HttpClientModule ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA ],
+
     })
     .compileComponents();
+
+    window.history.pushState({brandName: 'Starbucks'}, '', '');
 
     fixture = TestBed.createComponent(CardsComponent);
     component = fixture.componentInstance;
