992418dd4be21e88c44b9069fc54faf53bab6461
Jacob Simmons-Rosen <jsimmonsrosen@gmail.com>
2023-03-27 11:57:26 -0400

    Adds templates for user cards in dashboard

--- GiftCardXChange/src/app/dashboard/dashboard.component.ts
+++ GiftCardXChange/src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,10 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation, ViewChild } from '@angular/core';
 import { LoginComponent } from '../login/login.component';
 import { FormControl } from '@angular/forms';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { CARDS } from '../mock-cards';
 
 @Component({
   selector: 'app-dashboard',
@@ -11,14 +15,30 @@ import { FormControl } from '@angular/forms';
 export class DashboardComponent {
   myControl: FormControl = new FormControl();
 
+  cards = CARDS;
+
+  columnsToDisplay: string[] = ['amount', 'expiryDate'];
+  dataSource = new MatTableDataSource(CARDS);
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
   options = [
     'Starbuck',
     'BestBuy',
     'Target',
     'Kohls'
-   ];
+  ];
 
-
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
 
 
 }
