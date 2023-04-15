15222d6adb735c7e530b2ecef2b8905272edf454
Jacob Simmons-Rosen <jsimmonsrosen@gmail.com>
2023-04-15 17:27:33 -0400

    Adds reject dialog component

---
+++ GiftCardXChange/src/app/reject-dialog/reject-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reject-dialog',
+  templateUrl: './reject-dialog.component.html',
+  styleUrls: ['./reject-dialog.component.css']
+})
+export class RejectDialogComponent {
+
+}
