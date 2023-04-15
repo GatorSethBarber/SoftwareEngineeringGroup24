a79979c25ffbd926bb486b9234a35d25f2b1f4b7
Jacob Simmons-Rosen <jsimmonsrosen@gmail.com>
2023-04-15 17:27:20 -0400

    Adds accept dialog component

---
+++ GiftCardXChange/src/app/accept-dialog/accept-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accept-dialog',
+  templateUrl: './accept-dialog.component.html',
+  styleUrls: ['./accept-dialog.component.css']
+})
+export class AcceptDialogComponent {
+
+}
