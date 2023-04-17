ab537349150e65b1782de934c1584128bf3f8569
Jacob Simmons-Rosen <jsimmonsrosen@gmail.com>
2023-04-17 15:07:27 -0400

    WIP for dialog queries

--- GiftCardXChange/src/app/cancel-dialog/cancel-dialog.component.ts
+++ GiftCardXChange/src/app/cancel-dialog/cancel-dialog.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { AuthService } from '../auth.service';
 
 
 @Component({
@@ -8,5 +9,18 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
   styleUrls: ['./cancel-dialog.component.css']
 })
 export class CancelDialogComponent {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: { requestedID: number, offeredID: number }) { }
+  constructor(@Inject(MAT_DIALOG_DATA) public data: { requestedID: number, offeredID: number }, private AuthService: AuthService) { }
+
+  // cancelSwap() {
+  //   this.AuthService.denySwap([]).subscribe(
+  //     (res) => {
+  //       console.log(res);
+  //       alert('Card added successfully');
+  //     },
+  //     (err) => {
+  //       console.error(err);
+  //       alert('Error while adding the card');
+  //     }
+  //   );
+  // }
 }
