4c5979f1fa44e6e7323c14698e93c2ab7b5e29a1
Jacob Simmons-Rosen <jsimmonsrosen@gmail.com>
2023-04-17 17:08:34 -0400

    Adds swap rejecting functionality

--- GiftCardXChange/src/app/reject-dialog/reject-dialog.component.ts
+++ GiftCardXChange/src/app/reject-dialog/reject-dialog.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { AuthService } from '../auth.service';
 
 
 @Component({
@@ -8,5 +9,19 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
   styleUrls: ['./reject-dialog.component.css']
 })
 export class RejectDialogComponent {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: { requestedID: number, offeredID: number }) { }
+  constructor(@Inject(MAT_DIALOG_DATA) public data: { requestedID: number, offeredID: number }, private AuthService: AuthService) { }
+
+  denySwap() {
+    this.AuthService.denySwap(this.data.offeredID, this.data.requestedID).subscribe(
+      (res) => {
+        console.log(res);
+        alert('Card swap denied successfully');
+        location.reload();
+      },
+      (err) => {
+        console.error(err);
+        alert('Error while denying card swap');
+      }
+    );
+  }
 }
