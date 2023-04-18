95e58a6b18c7748e98afce29a40c42e437067651
vitran17 <vitran@ufl.edu>
2023-04-17 22:02:01 -0400

    added close button on request form

--- GiftCardXChange/src/app/dialogoption/dialogoption.component.ts
+++ GiftCardXChange/src/app/dialogoption/dialogoption.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+
+import { Component, Inject } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { Card } from '../card';
 import { MatTableDataSource } from '@angular/material/table';
@@ -8,48 +9,50 @@ import { Router } from '@angular/router';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import {ViewEncapsulation, ViewChild } from '@angular/core';
-import { FormBuilder , FormControl} from '@angular/forms';
-
-
-
-export interface CardRE {
-  company__name: string;
-  card__number: string;
-  card__amount: number;
+import { FormBuilder , FormControl, Validators} from '@angular/forms';
+import { SelectionModel } from '@angular/cdk/collections';
+import { CardsComponent } from '../cards/cards.component';
+import { Brand } from '../brand';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
+import { MatDialogRef } from '@angular/material/dialog';
+interface c {
+  cardID: number;
+  company: string;
+  username: string;
+  expirationDate: string;
+  amount: number;
 }
+
 @Component({
   selector: 'app-dialogoption',
   templateUrl: './dialogoption.component.html',
   styleUrls: ['./dialogoption.component.css']
 })
 
-
-
 export class DialogoptionComponent {
-userCard: Card
+
 user: User
-cards = CARDS;
 
-displayedColumns = ['Company', 'Card Number', 'Amount', 'Action'];
+cardID: number
 
-requests = [
-  CARDS, CARDS
-]
+displayedColumns = ['c', 'cn','am','ac'];
 
 
-columnsToDisplay: string[] = ['company', 'cardNumber', 'amount', 'expirationDate'];
-inboundColumnsToDisplay: string[] = ['requester', 'company', 'amount', 'expirationDate', 'requested'];
-outboundColumnsToDisplay: string[] = ['requested', 'company', 'amount', 'expirationDate', 'offeredCard'];
-dataSource = new MatTableDataSource(CARDS);
-inboundRequestSource = new MatTableDataSource(this.requests);
-outboundRequestSource = new MatTableDataSource(this.requests);
+myCardd: any[] = []
+
 
 @ViewChild(MatPaginator) paginator: MatPaginator;
 @ViewChild(MatSort) sort: MatSort;
+ 
 
-clickedRows = new Set<CardRE>();
 
-constructor(private formBuilder:FormBuilder,private AuthService: AuthService, private router: Router) {
+constructor(public dialogRef: MatDialogRef<DialogoptionComponent>,
+  private formBuilder:FormBuilder,private AuthService: AuthService, private router: Router, 
+  @Inject(MAT_DIALOG_DATA) public data: {cardID:number}) {
+    this.cardID = data.cardID;
+    console.log(this.cardID)
   AuthService.user$.subscribe(
     (user) =>
     (this.user = user ?? {
@@ -60,46 +63,46 @@ constructor(private formBuilder:FormBuilder,private AuthService: AuthService, pr
       username: '',
     })
   );
-}
-
-applyFilter(event: Event) {
-  const filterValue = (event.target as HTMLInputElement).value;
-  this.dataSource.filter = filterValue.trim().toLowerCase();
-}
 
 
 
-options = ['Starbuck', 'BestBuy', 'Target', 'Kohls'];
-
-
-ngAfterViewInit() {
-  this.dataSource.paginator = this.paginator;
-  this.dataSource.sort = this.sort;
-}
-
-ngOnInit() {
   this.AuthService.userCards(this.user).subscribe(
     (res) => {
-      this.dataSource = res;
-    },
-    (err) => alert('Error getting card for user: ' + "Anlaf")
-  );
-    console.log(this.requests);
-}
-
-
-
-requestSubmit(){
-  this.AuthService.sendRequest().subscribe((res) => {
-    console.log(res);
-    alert('Sent request');
-  },
-  (err) => {
-    alert('please checkk');
-  }
+      this.myCardd = res;
+    console.log('list of my card', this.myCardd)
+    }
   )
 }
 
 
 
+cardRequestFrom = this.formBuilder.group({
+  company: ['', [Validators.required]],
+  cardnumber: ['', [Validators.required]],
+  amount: ['', [Validators.required]],
+})
+
+//first Card is my card, and second one is other card
+onSubmitted(){
+  console.log(this.cardRequestFrom.get('cardnumber')?.value)
+  const myIDCARD: any = {
+    cardID : this.cardRequestFrom.get('cardnumber')?.value
+  }
+  console.log(this.cardID)
+this.AuthService.sendRequest(this.cardID, myIDCARD.cardID).subscribe(
+  (res) => {
+    alert('Request submitted successfully');
+  },
+  (err) => alert('hmmhmm something wrong')
+  );
+
 }
+
+onCloseClick(): void {
+  this.dialogRef.close();
+}
+
+}
+
+
+
